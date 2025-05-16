ce87bb846e1a73a013ed029b770393a96b4404bd
Juan Carlos Chavez Garcia <juan.chavez@aforecoppel.com>
2025-05-15 18:04:33 -0700

    se versiona metodo de buscar por id

--- ZonaFit/src/main/java/zona_fit/datos/ClienteDAO.java
+++ ZonaFit/src/main/java/zona_fit/datos/ClienteDAO.java
@@ -43,6 +43,29 @@ public class ClienteDAO implements IClienteDAO{
 
     @Override
     public boolean buscarClientePorId(Cliente cliente) {
+        PreparedStatement ps;
+        ResultSet rs;
+        var con = getConexion();
+        var sql = "SELECT * FROM gmcliente WHERE id = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, cliente.getId());
+            rs = ps.executeQuery();
+            if(rs.next()){
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setMembresia(rs.getInt("membresia"));
+                return true;
+            }
+        }catch (Exception e){
+            System.out.println("Error al recuperar cliente por id: " + e.getMessage());
+        }finally {
+            try {
+                con.close();
+            }catch (Exception e){
+                System.out.println("Error al cerrar la conexion " + e.getMessage());
+            }
+        }
         return false;
     }
 
@@ -62,10 +85,24 @@ public class ClienteDAO implements IClienteDAO{
     }
 
     public static void main(String[] args) {
-        // Listar clientes
-        System.out.println("*** Listar Clientes ***");
+
         IClienteDAO clienteDao = new ClienteDAO();
-        var clientes = clienteDao.listarClientes();
-        clientes.forEach(System.out::println);
+        // Listar clientes
+        //System.out.println("*** Listar Clientes ***");
+
+        //var clientes = clienteDao.listarClientes();
+        //clientes.forEach(System.out::println);
+
+        //Buscar por ID
+        var cliente1 = new Cliente(3);
+        System.out.println("Cliente antes de la busqueda: " + cliente1);
+
+        var encontrado = clienteDao.buscarClientePorId(cliente1);
+        if(encontrado){
+            System.out.println("Cliente encontrado: "+ cliente1);
+        }else{
+            System.out.println("No se encontro registro: "+ cliente1);
+        }
+
     }
 }
